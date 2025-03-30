35e6b486a295d33dd34b95abc7d2d2195343243a
NHelixo <sasasas123sasasasa@gmail.com>
2025-03-30 09:57:58 +0300

    update

--- game.py
+++ game.py
@@ -2,19 +2,13 @@ import pygame
 from player import *
 from map import *
 
+
 player = Player(100, 5, 4, 100, 100)
 map = Map()
 
 running = True
 while running:
     screen.fill((0, 0, 0))
-
-    # # Копія поточної позиції
-    # new_player = player.copy()
-
-    # # Перевіряємо колізії зі стінами
-    # if not any(new_player.colliderect(wall) for wall in map.walls):
-    #     player = new_player  # Якщо немає колізії, оновлюємо позицію
 
     player.run()
     map.generating_map()
@@ -27,3 +21,4 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ import pygame
 
 
 class Player:
-    def __init__(self, health, damage, speed, x, y,):
+    def __init__(self, health, damage, speed, x, y):
         self.anim_count_walk = 0
         self.health = health
         self.damage = damage
@@ -25,13 +25,6 @@ class Player:
             self.y += self.speed
 
         screen.blit(player, (self.x, self.y))
-
-    # def colliderect(self, other):
-    #     return self.rect.colliderect(other)  # Перевірка колізії з іншим rect
-
-    # def copy(self):
-    #     # Створюємо новий об'єкт Player з тією самою позицією та іншими атрибутами
-    #     return Player(self.x, self.y, self.health, self.damage, self.speed)
 
     def fire(self):
         pass
